43f1a32b217428ce7a844369da53ace9b86295ba
Zaneta Gebka <43746213+zanetagebka@users.noreply.github.com>
2020-04-03 11:52:11 +0200

    add possibility to create folder

--- script/spreadsheet.rb
+++ script/spreadsheet.rb
@@ -1,25 +1,28 @@
-require "google/apis/docs_v1"
-require "googleauth"
-require "googleauth/stores/file_token_store"
-require "fileutils"
+# frozen_string_literal: true
+
+require 'google/apis/docs_v1'
+require 'google/apis/cloudresourcemanager_v2'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'fileutils'
 require 'bundler'
 Bundler.require
 
-OOB_URI = "urn:ietf:wg:oauth:2.0:oob".freeze
-APPLICATION_NAME = "Google Docs API Ruby Quickstart".freeze
-CREDENTIALS_PATH = "script/credentials.json".freeze
-TOKEN_PATH = "token.yaml".freeze
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'Google Docs API Ruby Quickstart'
+CREDENTIALS_PATH = 'script/credentials.json'
+TOKEN_PATH = 'token.yaml'
 SCOPE = [Google::Apis::DocsV1::AUTH_DRIVE, Google::Apis::DriveV3::AUTH_DRIVE_FILE]
 
 def authorize
   client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
   token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
   authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
-  user_id = "default"
+  user_id = 'default'
   credentials = authorizer.get_credentials user_id
   if credentials.nil?
     url = authorizer.get_authorization_url base_url: OOB_URI
-    puts "Open the following URL in the browser and enter the " \
+    puts 'Open the following URL in the browser and enter the ' \
          "resulting code after authorization:\n" + url
     code = gets
     credentials = authorizer.get_and_store_credentials_from_code(
@@ -35,33 +38,31 @@ service = Google::Apis::DocsV1::DocsService.new
 service.client_options.application_name = APPLICATION_NAME
 service.authorization = authorize
 
-print "Google document id:\n"
-
-document_id = gets.chomp
-service.get_document document_id
-
 session = Google::Apis::DriveV3::DriveService.new
 session.authorization = authorize
 
 drive = Google::Apis::DriveV3::File.new
 
-copied = session.copy_file(document_id)
+### Main Script Part
+print "Enter Your Google document id:\n"
+document_id = gets.chomp
+service.get_document document_id
 
-# file = session.create_drive(copied.id, file_metadata)
-# puts file.id
+copied = session.copy_file("#{document_id}")
 
-# drive = Google::Apis::DriveV3::File.new
-# drive.authorization = authorize
-#
-# drive.metadata
+###CREATION OF FOLDER
+drive.name = copied.name
+drive.mime_type = "application/vnd.google-apps.folder"
+folder = session.create_file(drive, fields: 'id')
 
-print "replace text:\n"
+###UPDATE OF TEXT FROM DOC
+print "What text you want to replace:\n"
 to_replace = gets.chomp
-print "replace to:\n"
+print "What you want to enter instead:\n"
 replaced = gets.chomp
-text1 = Google::Apis::DocsV1::SubstringMatchCriteria.new(text: "#{to_replace}")
-req1 = Google::Apis::DocsV1::ReplaceAllTextRequest.new(contains_text: text1, replace_text: "#{replaced}")
 
-replacement_requests = [Google::Apis::DocsV1::Request.new(replace_all_text: req1)]
+text = Google::Apis::DocsV1::SubstringMatchCriteria.new(text: to_replace.to_s)
+request = Google::Apis::DocsV1::ReplaceAllTextRequest.new(contains_text: text, replace_text: replaced.to_s)
+replacement_requests = [Google::Apis::DocsV1::Request.new(replace_all_text: request)]
 batch_request = Google::Apis::DocsV1::BatchUpdateDocumentRequest.new(requests: replacement_requests)
 service.batch_update_document(copied.id, batch_request)
